7c6675adcffc34debc93dbefa370a7857bda5f5f
Leonie Dong <leoniedong@hotmail.com>
2019-12-20 14:40:41 +0800

    removed file

--- config/initializers/active_model_serializers.rb
+++
@@ -1,7 +0,0 @@
-ActiveModelSerializers.config.tap do |c|
-    c.adapter = :json_api
-    c.jsonapi_include_toplevel_object = true
-    c.jsonapi_version = "1.0"
-end
-
-# This tells AMS to use the :json_api serialization adapter, to include a top-level object, and to adhere to v1.0 of the JSON:API specification
